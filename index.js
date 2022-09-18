27ec0e13f1bd6dcc4cdd25acb4f35722f79959f4
Luis Leal <luisrrleal@gmail.com>
2022-09-17 22:12:17 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.static("public"));
+app.use("/api/auth", require('./routes/auth'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+/*
+host + api/auth
+*/
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+module.exports = router;
