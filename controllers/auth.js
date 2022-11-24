9e071f02f0dee2feed4d696b22c156d4ebe8b5c6
Luis Leal <luisrrleal@gmail.com>
2022-11-23 19:56:03 -0600

    Connect to db and hashing passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,29 @@
 const { response } = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res = response) => {
   try {
-    const user = new User(req.body);
+    const { mail, password } = req.body;
+
+    let user = await User.findOne({ mail });
+    if (user)
+      res.status(400).json({
+        ok: false,
+        msg: "Someone already is using this email",
+      });
+
+    user = new User(req.body);
+    //Hashing password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: "New user created correctly",
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
     console.log(error);
