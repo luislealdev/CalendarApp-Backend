c23ec3bafed147ee32125b39d7330ac2ceb78655
Luis Leal <luisrrleal@gmail.com>
2022-12-07 20:24:45 -0600

    Add cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+var cors = require("cors");
 const { connectDatabase } = require("./dataBase/config");
 require("dotenv").config();
 
 //Create app
 const app = express();
+//Add cors (Security)
+app.use(cors());
 //Connect db
 connectDatabase();
 //Use public folder
 app.use(express.static("public"));
 app.use(express.json());
-app.use("/api/auth", require('./routes/auth'));
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
