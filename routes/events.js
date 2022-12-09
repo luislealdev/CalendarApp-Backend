2ce36a49159520b615f4b402b7fcd3bbe1045764
Luis Leal <luisrrleal@gmail.com>
2022-12-08 21:01:56 -0600

    Add model and validators

--- routes/events.js
+++ routes/events.js
@@ -1,18 +1,30 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { isDate } = require("../helpers/isDate");
 const {
   getEvents,
   createEvent,
   updateEvent,
   deleteEvent,
 } = require("../controllers/events");
+const { validarCampos } = require("../middlewares/validarCampos");
 const { validarJWT } = require("../middlewares/validarJWT");
 
 const router = Router();
-router.use(validarJWT)
+router.use(validarJWT);
 
 router.get("/", getEvents);
 
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("start", "Start date is required").custom(isDate),
+    check("end", "End date is required").custom(isDate),
+    validarCampos,
+  ],
+  createEvent
+);
 
 router.put("/:id", updateEvent);
 
